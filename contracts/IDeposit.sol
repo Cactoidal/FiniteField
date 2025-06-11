a0d8c7c7c111474f8b0b00b87afdeff4ba829991
Cactus <115384394+Cactoidal@users.noreply.github.com>
2025-06-11 12:56:28 -0400

    Delete contracts/IDeposit.sol

--- contracts/IDeposit.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// An example of a consumer contract that directly pays for each request.
-pragma solidity 0.8.20;
-
-interface IDeposit {
-
-    function depositGameToken(address depositorContract, address playerAddress, uint256 amount) external;
-
-}
