4f05df9b59a344677edcdbee97580bec12556ea8
Cactus <115384394+Cactoidal@users.noreply.github.com>
2025-06-11 12:59:46 -0400

    Create ZKPVerifier.sol

---
+++ contracts/verifiers/ZKPVerifier.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// An example of a consumer contract that directly pays for each request.
+pragma solidity 0.8.20;
+
+
+import {Groth16HandVerifier} from "./HandVerify.sol";
+//import {Groth16VSwapVerifier} from "./SwapVerify.sol";
+//import {Groth16VPlayVerifier} from "./PlayVerify.sol";
+
+
+contract ZKPVerifier is Groth16HandVerifier {
+
+// Aggregates verification contracts
+
+}
