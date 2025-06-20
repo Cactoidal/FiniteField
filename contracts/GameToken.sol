197e2de18ff99d22401265f7a6518f056170e55f
Cactus <115384394+Cactoidal@users.noreply.github.com>
2025-06-20 00:03:18 -0400

    Update GameToken.sol

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// An example of a consumer contract that directly pays for each request.
 pragma solidity 0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
