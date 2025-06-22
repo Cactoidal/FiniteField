d09ec9935a5163e44afef842898b17e5a0aa4da9
Cactus <115384394+Cactoidal@users.noreply.github.com>
2025-06-21 21:06:04 -0400

    Delete contracts/interfaces/IVRFWrapper.sol

--- contracts/interfaces/IVRFWrapper.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-interface IVRFWrapper {
-
- function estimateRequestPriceNative(uint32 _callbackGasLimit, uint32 _numWords, uint256 _requestGasPriceWei) 
-    external view returns (uint256); 
-
-}
